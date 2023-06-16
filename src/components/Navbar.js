9aea66deb988050904d1976edee772f5c126d45a
Tyler Hoh <tylerhoh@gmail.com>
2023-06-15 21:07:47 -0700

    Commented out links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import "../css/Navbar.css";
 const Navbar=()=>{
     return(
         <nav>
             <div className="button-row">
-                <Link className="button" to="/projects">Projects</Link>
+                {/* <Link className="button" to="/projects">Projects</Link>
                 <Link className="button" to="/reading">Reading</Link>
                 <Link className="button" to="/resume">Resume</Link>
-                <Link className="button" to="/thoughts">Thoughts</Link>
+                <Link className="button" to="/thoughts">Thoughts</Link> */}
             </div>
         </nav>
     )
